b652ec36abe85c6fb4d9963070937c09d71baf90
123木头人 <1206993746@qq.com>
2022-03-26 20:48:48 +0800

    🦄 refactor(布局): 添加默认布局

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <Header />
+    <main class="space-y-12 mt-10">
+      <slot />
+    </main>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,27 @@
 <script lang="ts" setup>
+  definePageMeta({
+    layout: 'default',
+  });
   // const { data } = await useFetch('https://api.123mtr.top/menu/findAll')
 
-  import request from '~/utils/request';
-  const { data } = await request('/menu/findAll').get().json();
+  // import request from '~/utils/request';
+  // const { data } = await request('/menu/findAll').get().json();
+  // console.log('🚀 ~ file: Header.vue ~ line 4 ~ data', data.value);
+
+  const { data } = await useFetch('https://api.123mtr.top/menu/findAll');
   console.log('🚀 ~ file: Header.vue ~ line 4 ~ data', data.value);
 </script>
 
 <template>
-  <div>
-    <Header />
-    <main class="space-y-12 mt-10">
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Paging />
-    </main>
-  </div>
+  <NuxtLayout>
+    <Article />
+    <Article />
+    <Article />
+    <Article />
+    <Article />
+    <Article />
+    <Article />
+    <Article />
+    <Paging />
+  </NuxtLayout>
 </template>
